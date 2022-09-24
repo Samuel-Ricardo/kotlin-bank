71be3568f322a0bb431791cb359bde7fcd431612
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-24 15:57:16 -0300

    :sparkles: create: fun - create (AccountController)

--- bankaccount/src/main/kotlin/com/example/bankaccount/AccountController.kt
+++ bankaccount/src/main/kotlin/com/example/bankaccount/AccountController.kt
@@ -1,10 +1,19 @@
 package com.example.bankaccount
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("accounts")
 class AccountController(val repository: AccountRepository) {
+
+    @PostMapping
+    fun create(@RequestBody account: Account) = ResponseEntity.ok(repository.save(account));
 
 }
